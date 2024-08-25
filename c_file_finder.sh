070934ac90c22c574c613c77aeff67999e36d069
engrron30 <ronaldtorresrosete30@gmail.com>
2024-08-25 14:06:00 +0800

    Ver2.0 Adds AWK printing for permissions to print each character in owner permission.

--- c_file_finder.sh
+++ c_file_finder.sh
@@ -12,7 +12,17 @@ echo "\n------------------------------------------------------------------------
 echo "C files are shown below:"
 ls -l | grep *.c | awk '{print "File Name:\t"$9	"\nAuthor:\t\t" $3	"\nPermissions:\t" $1}'
 
-DUMMY_VAR_1=$(ls -l | grep *.c | awk '{print "Permissions:\t" $1}' )
+OWNER_COMMAND=$(ls -l | grep *.c)
+OWNER_PERMSN=$($COMMAND_1)
+
+OWNER_READ=$(	echo $OWNER_COMMAND | 	awk '{print substr($1, 2, 1)}')
+OWNER_WRITE=$(	echo $OWNER_COMMAND | 	awk '{print substr($1, 3, 1)}')
+OWNER_EXEC=$(	echo $OWNER_COMMAND | 	awk '{print substr($1, 4, 1)}')
+
+echo "\nFile Permissions for Owner:"
+echo "Read:\t\t$OWNER_READ"
+echo "Write:\t\t$OWNER_WRITE"
+echo "Execute:\t$OWNER_EXEC"
 
 echo "\n---------------------------------------------------------------------------------------"
 echo "Script is done!\n\n"
---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	printf("Script created by Ronald\n\n");
+	printf("Hello, World!\n");
+
+	return 0;
+}
